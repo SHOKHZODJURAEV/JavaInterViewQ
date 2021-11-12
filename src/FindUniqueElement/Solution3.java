afba2e3004d8208462c96fe87f7dfedeca55beb1
SHOKHZODJURAEV <shahaking9393@gmail.com>
2021-11-11 22:18:52 -0500

    unique part is done

--- src/FindUniqueElement/Solution3.java
+++ src/FindUniqueElement/Solution3.java
@@ -1,11 +1,22 @@
 package FindUniqueElement;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Solution3 {
 
     public static void main(String[] args) {
 
 
+        String str1 = "AAABBBCDDDDEEEDGGGH";
 
+        String result = "";
+
+        for (String each:str1.split("")) {
+            result += ((Collections.frequency(Arrays.asList(str1.split("")),each))==1)?each:"";
+
+        }
+        System.out.println("result = " + result);
 
 
     }
